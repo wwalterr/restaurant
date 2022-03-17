409e98efb3a5f6e2a96c0aa53c55abecac5b3320
Hydroxion <whenrique26@gmail.com>
2022-03-17 15:47:28 -0300

    TDD scopes

--- packages/server/application.test.js
+++ packages/server/application.test.js
@@ -2,4 +2,30 @@ import request from "supertest";
 
 import server from "./application";
 
-it("should run", () => {});
+describe("restaurant", () => {
+  it("defines open hours", () => {});
+
+  it("updates open hours", () => {});
+
+  it("return open hours", () => {});
+});
+
+describe("tables", () => {
+  it("creates a table", () => {});
+
+  it("updates a table", () => {});
+
+  it("find a table", () => {});
+
+  it("return all tables", () => {});
+});
+
+describe("appointments", () => {
+  it("creates an appointment", () => {});
+
+  it("updates an appointment", () => {});
+
+  it("finds an appointment", () => {});
+
+  it("return all appointments", () => {});
+});
